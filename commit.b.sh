ac825456489cb6053a542b36999be0c0c1430b09
bussiere <bussiere@gmail.com>
2021-12-06 13:01:41 +0100

    wip master Updated: 2021-12-06 13:01:41

---
+++ commit.b.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git config --global user.name "bussiere"
+git config --global user.email "bussiere@gmail.com"
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+git pull origin $branch
+git add .
+current="`date +'%Y-%m-%d %H:%M:%S'`"
+msg="Updated: $current"
+git commit -m "wip $branch $msg"
+git push origin $branch
+
